d16cc6d4dfb448c85891d85b222c34b414c44d8f
andela-gochieng <gertrude.ochieng@andela.com>
2018-03-27 15:10:22 +0300

    Minor fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const paths = {
   DIST: path.resolve(__dirname, 'dist'),
@@ -20,6 +21,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(paths.SRC, 'index.html'),
     }),
+    new ExtractTextPlugin('style.bundle.css'),
   ],
   module: {
     rules: [
@@ -30,6 +32,12 @@ module.exports = {
           'babel-loader',
         ],
       },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader',
+        }),
+      }
     ],
   },
   resolve: {
